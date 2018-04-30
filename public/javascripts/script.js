84b0caeba1d025809ae1d4060c92937be2ec6fe6
Rujool <rujool@gmail.com>
2018-04-30 18:06:08 -0500

    Some optimizations

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -15,32 +15,37 @@ app.config(function($routeProvider,$sceDelegateProvider) {
 
 
 app.controller('mainController',['$scope','numService',function($scope,numService){
-    $scope.getRandomNumbers = function(){
-        var url = 'https://www.random.org/integers/?num=2&min=1&max=1000000000&col=1&base=10&format=plain&rnd=new';
-        var p,q,lambda,e=bigInt(65537);
+    $scope.genKeys = function(){
+        var url = 'https://www.random.org/integers/?num=10000&min=1&max=1000000000&col=1&base=10&format=plain&rnd=new';
+        var p,q,lambda,e=bigInt(65537);     // Fixed e
         $scope.halted = false;
         $scope.buttonClicked = true;
-        var counter = 0;
+        var found = false;
         numService.getNumbers(url).then(function(res){
-            counter+=1;
-            p = bigInt(res.split('\n')[0]);
-            q = bigInt(res.split('\n')[1]);
-            lambda = bigInt.lcm(p.minus(1), q.minus(1));
-            if(p%2 == 0 || q%2 == 0 || !p.isProbablePrime() || !q.isProbablePrime()
-              || bigInt.gcd(e, lambda).notEquals(1)){           // check gcd(lambda,e) == 1
-                if(counter===Number.MAX_SAFE_INTEGER){
-                    $scope.halted = true;
-                    $scope.error_message = "Could not generate primes. Please try again!";
+            var resArr = res.split('\n');
+            for(var i = 0; i < 9999 && !found; i++){
+                p = bigInt(resArr[i]);
+                for(var j = i+1; j < 10000 && !found; j++){
+                    q = bigInt(resArr[j]);
+                    lambda = bigInt.lcm(p.minus(1), q.minus(1));
+                    console.log(p,q);
+                    if(p%2 == 0 || q%2 == 0 || !p.isPrime() || !q.isPrime()
+                        || bigInt.gcd(e, lambda).notEquals(1)){           // check gcd(lambda,e) == 1
+                        continue;
+                    }
+                    else{
+                        $scope.halted = true;
+                        $scope.p = p;
+                        $scope.q = q;
+                        $scope.n = p.multiply(q);
+                        $scope.e = e;
+                        $scope.d = e.modInv(lambda);
+                        found = true;
+                    }
                 }
-                $scope.getRandomNumbers();
             }
-            else{
-                $scope.halted = true;
-                $scope.p = p;
-                $scope.q = q;
-                $scope.n = p.multiply(q);
-                $scope.e = e;
-                $scope.d = e.modInv(lambda);
+            if(!found){
+                $scope.getRandomNumbers();
             }
         }, function(err){
             console.log(err);
